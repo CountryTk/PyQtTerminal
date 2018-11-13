3d0f13bb18f811aea2f8eb0f64c887a576c57b6d
Fuchsiaff <31344416+Fuchsiaff@users.noreply.github.com>
2018-11-13 20:43:58 +0200

    This is now a fair example of something

--- Untitled_file_61.py
+++ Untitled_file_61.py
@@ -16,20 +16,21 @@ class PlainTextEdit(QPlainTextEdit):
     commandSignal = pyqtSignal(str)
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.name = ("[" + str(getpass.getuser()) + "@" + str( socket.gethostname()) + "]" + "   ~/" + str(os.path.basename(os.getcwd())) + " >$ ")
-        # self.name = "abc"
+        self.name = "[" + str(getpass.getuser()) + "@" + str(socket.gethostname()) + "]" + "  ~" + str(os.getcwd()) + " >$ "
         self.appendPlainText(self.name)
         self.parent = parent
 
     def keyPressEvent(self, e):
         cursor = self.textCursor()
+        
         if self.parent:
             if e.key() == 16777220:
                 text = self.textCursor().block().text()
                 command = text.replace(self.name, "")
-                # TODO: RUN COMMAND
+                command_list = command.split()
                 self.commandSignal.emit(command)
                 self.appendPlainText(self.name)
+               
                 return
                 
             if e.key() == 16777219:
@@ -67,6 +68,7 @@ class Terminal(QWidget):
         self.editor.appendPlainText(self.error.strip('\n'))
 
         self.errorSignal.emit(self.error)
+        
         if self.error == "":
             pass
         else:
@@ -87,23 +89,31 @@ class Terminal(QWidget):
         self.process.start(command)
         
     def handle(self, command):
-        """
         
-        split a command into list so command `echo hi` would appear as ['echo', 'hi']
+        """Split a command into list so command `echo hi` would appear as ['echo', 'hi']"""
         
-        """
         command_list = command.split()
         
-        """Now we start defining our own commands"""
+        """Now we start implementing some commands"""
         
         if command == "clear":
             self.editor.clear()
             
-        if command_list[0] == "echo":
+        elif command_list[0] == "echo":
             self.editor.appendPlainText(" ".join(command_list[1:]))
             
-        if command == "exit":
+        elif command == "exit":
             qApp.exit()
+        
+        elif command_list[0] == "cd":
+            try:
+                os.chdir(" ".join(command_list[1:]))
+                self.editor.name = "[" + str(getpass.getuser()) + "@" + str( socket.gethostname()) + "]" + "  ~" + str(os.getcwd()) + " >$ "
+            except FileNotFoundError as E:
+                self.editor.appendPlainText(str(E))
+            
+        else:
+            self.run(command)    
     
 if __name__ == "__main__":
     app = QApplication(sys.argv)
